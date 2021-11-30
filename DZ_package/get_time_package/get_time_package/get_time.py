0f4c038dca20506c581bb2575d5086130c3ffa77
fifijoner <fifijoner73@gmail.com>
2021-11-30 13:08:14 +0300

    3_point

--- DZ_package/get_time_package/get_time_package/get_time.py
+++ DZ_package/get_time_package/get_time_package/get_time.py
@@ -1,13 +1,24 @@
+import sys
 import requests
+try:
+    from pretty_print_package import pretty_print 
+except ImportError:
+    pass
 
 def get_time():
     resp = requests.get("http://worldtimeapi.org/api/timezone/Europe/Moscow")
     unixtime = resp.json()["unixtime"]
     return unixtime
 
+def print_time(unix):
+    print(unix)
+
 def main():
     unixtime = get_time()
-    print(unixtime)
+    if 'pretty_print_package' in sys.modules:
+        pretty_print.print_time_pretty(unixtime)
+    else:
+        print_time(unixtime)
 
 if __name__ == '__main__':
     main()
